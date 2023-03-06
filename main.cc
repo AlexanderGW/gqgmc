39cde6707a9be4e9c18ea67ad07eb7f15e54e976
alexandergw <alexgw@gmail.com>
2023-03-06 17:24:01 +0000

    Switch to CSV output

--- main.cc
+++ main.cc
@@ -34,7 +34,7 @@ void signalHandler(int signum) {
 void outMessage(string msg) {
   std::time_t t
     = std::chrono::system_clock::to_time_t(std::chrono::system_clock::now());
-  std::cout << std::put_time( std::localtime( &t ), "%FT%T%z" ) << "; " << msg << endl;
+  std::cout << std::put_time( std::localtime( &t ), "%FT%T%z" ) << "," << msg << endl;
 }
 
 // Utility to encapsulate the code to display an error message. This is
